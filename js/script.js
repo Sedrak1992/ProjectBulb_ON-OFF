2dafb9112b2fe71a843a812dab59ddac5328db81
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-06-16 16:34:28 +0400

    aa

--- js/script.js
+++ js/script.js
@@ -1,89 +1,28 @@
 const app = document.querySelector(".app");
+
+app.innerHTML = `
+<div>
+    <h1>
+        Turn the light on and off
+        <br />
+        by clicking the bulb!
+    </h1>
+    <div  class="divBox">
+        <img src="img/OFFbulb.jpeg" alt="lamp" class=" iimg"/>
+        <img src="img/ONbulb.jpeg" alt="lamp" class="imgLampMiacrac" />
+    </div>
+</div>
+  `;
+
+
+
 const imgLalp = document.querySelector(".iimg");
 const imgLampMiacrac = document.querySelector(".imgLampMiacrac");
 
-
- function  crateTodo() {
-  imgLampMiacrac.classList.toggle("dooo");
+imgLalp.onclick = function () {
+  imgLampMiacrac.classList.toggle("dexin");
 };
 
-
-
-imgLalp.onclick = crateTodo;
-imgLampMiacrac.onclick = crateTodo;
-
-
-
-
-
-
-
-
-
-
-
-// crateTodo();
-// function box() {
-//   app.innerHTML += `
-//       <h1>
-//          Turn the light on and off
-//          <br />
-//          by clicking the bulb!
-//       </h1>
-//       <img src="img/OFFbulb.jpeg" alt="lamp" />
-//       <img src="img/ONbulb.jpeg" alt="lamp" class="imgLamp"/>
-//   `;
-// }
-
-// box();
-// const input = document.querySelector(".tudo_list_form_input");
-// const addBtn = document.querySelector(".tudo_list_form_button");
-// const listContainer = document.querySelector(".tudo_list__items__ul");
-
-// addBtn.addEventListener("click", crateTodo);
-
-// function removeTodo() {
-//   const listParent = this.parentElement.parentElement;
-//   listContainer.removeChild(listParent);
-// }
-
-// function doneTodo() {
-//   const listParent = this.parentElement.parentElement;
-//   listParent.classList.toggle("done");
-// }
-
-// function setActionsEvents() {
-//   const deleteBtns = document.querySelectorAll(
-//     ".tudo_list__items__row__button--remove"
-//   );
-
-//   const doneBtns = document.querySelectorAll(
-//     ".tudo_list__items__row__button--done"
-//   );
-
-//   deleteBtns.forEach(function (btn) {
-//     btn.addEventListener("click", removeTodo);
-//   });
-
-//   doneBtns.forEach(function (btn) {
-//     btn.addEventListener("click", doneTodo);
-//   });
-// }
-
-// function crateTodo() {
-//   if (input.value.length <= 3) {
-//     return alert("miqich erkar ban gri");
-//   }
-//   listContainer.innerHTML += `
-//   <li class="tudo_list__items__row">
-//   <span class="tudo_list__items__row__content">${input.value}</span>
-//   <div>
-//     <button class="tudo_list__items__row__button--done">add</button>
-//     <button class="tudo_list__items__row__button--remove">X</button>
-//   </div>
-// </li>
-//   `;
-//   input.value = "";
-
-//   setActionsEvents();
-// }
+imgLampMiacrac.onclick = function () {
+  imgLampMiacrac.classList.toggle("dexin");
+};
